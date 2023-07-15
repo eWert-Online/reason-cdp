91d17c1e2c30d4543fa4a006c143a1ff27abe07c
eWert-Online <eWert-Online@users.noreply.github.com>
2023-07-15 05:08:45 +0000

    Roll protocol to r1170846

--- lib/Commands.ml
+++ lib/Commands.ml
@@ -29182,7 +29182,11 @@ module Fetch = struct
      is paused in the Response stage and is mutually exclusive with
      takeResponseBodyForInterceptionAsStream. Calling other methods that
      affect the request or disabling fetch domain before body is received
-     results in an undefined behavior. *)
+     results in an undefined behavior.
+     Note that the response body is not available for redirects. Requests
+     paused in the _redirect received_ state may be differentiated by
+     `responseCode` and presence of `location` response header, see
+     comments to `requestPaused` for details. *)
   module GetResponseBody = struct
     module Response : sig
       type result = {
